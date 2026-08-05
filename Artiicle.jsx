10c4c7fabe2ac837a46fa55360957acc8cee3a5b
GrassHopper132 <crhymer101@gmail.com>
2026-08-04 21:45:24 -0400

    Add Article component with props for rendering

--- Artiicle.jsx
+++ Artiicle.jsx
@@ -1,1 +1,12 @@
 
+export default function Article({ date, title, imageSrc, imageAlt, text }) {
+  return (
+    <article>
+      <time dateTime={date}>{date}</time>
+      <h2>{title}</h2>
+      <img src={imageSrc} alt={imageAlt} />
+      <p>{text}</p>
+      <a href="#" className="continue-link">continues...</a>
+    </article>
+  );
+}
