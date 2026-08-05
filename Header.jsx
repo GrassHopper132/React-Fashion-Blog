c1ba7cc51e7e3cde0466444f4aec1d3168e11f66
GrassHopper132 <crhymer101@gmail.com>
2026-08-04 21:43:12 -0400

    Add Header component with navigation

--- Header.jsx
+++ Header.jsx
@@ -1,1 +1,16 @@
 
+import Nav from './Nav';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Sartorialist's Journal</h1>
+      <h2>Better Dressed People</h2>
+      <Nav 
+        ariaLabel="Main Navigation" 
+        navClass="nav-main" 
+        anchorClass="nav-link-main" 
+      />
+    </header>
+  );
+}
